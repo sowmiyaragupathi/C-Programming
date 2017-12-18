aff99e528646243630c54faa75df0131983b88fa
sowmiyaragupathi <34641629+sowmiyaragupathi@users.noreply.github.com>
2017-12-18 14:48:54 +0530

    Create variablesstoring.c

---
+++ Beginner level/variablesstoring.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	char name[100]="Sachin Ramesh Tendulkar";
+	char nick_name[50]="Little Master";
+	int runs=18426;
+	float bat_avg=44.83;
+	int matches=463;
+	printf("%s\n %s\n %d\n %.2f\n %d",name,nick_name,runs,bat_avg,matches);
+	return 0;
+}
+
