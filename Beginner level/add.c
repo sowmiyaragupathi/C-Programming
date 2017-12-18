96d74215188117c37aff9fb4e169caab3446f8be
sowmiyaragupathi <34641629+sowmiyaragupathi@users.noreply.github.com>
2017-12-18 15:26:58 +0530

    Create add.c

---
+++ Beginner level/add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main ()
+{
+int a, b,c;
+float f;
+a = 10;
+b = 20;
+c = a + b;
+printf("value of c : %d ", c);
+f = 70.0/3.0;
+printf("value of f : %.2f ", f);
+return 0;
+}
